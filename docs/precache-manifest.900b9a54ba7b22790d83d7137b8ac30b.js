28166ffb8076e9254a9339c58ee789baa0ef5efb
Facundo <facundoh1986@gmail.com>
2020-10-05 17:10:51 +0200

    docs agregado

---
+++ docs/precache-manifest.900b9a54ba7b22790d83d7137b8ac30b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf3146552c0ae1266612a28304db3bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53bbad0b5011843ed90",
+    "url": "/static/css/main.56f28487.chunk.css"
+  },
+  {
+    "revision": "66e9ffe31672020b5776",
+    "url": "/static/js/2.c09b8120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c09b8120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53bbad0b5011843ed90",
+    "url": "/static/js/main.77a15e4a.chunk.js"
+  },
+  {
+    "revision": "055f3e008571789170d2",
+    "url": "/static/js/runtime-main.3e4d2dea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
